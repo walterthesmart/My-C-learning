299e1a0482e3263c06b59eb6bbdae46d99f30fd4
root <root@DESKTOP-R7S4241>
2023-02-16 13:58:56 +0100

    My C programming journey

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  Thius prints Hello Walter
+ * Return: This returns 0
+ */
+int main(void)
+{	int x; /*Declaration*/
+	x = 24; /*Initialization*/
+	int y = 45; /*Declaration + initialization*/
+	float z = 34.67;
+	char a = 'R';
+	char name[] = "Walter Nwaugo";
+
+	printf("My name is %s and I have $ %f", name, z);
+	printf("Hello Walter\n"); /* Inline comments. */
+	exit(0);
+	printf("Hello to you toon\n"); /*prints Hello to you too*/
+	return (0);
+
+}
