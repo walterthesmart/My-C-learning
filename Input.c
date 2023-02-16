7947cdf7e286e72552a80c528d01d9a8ea757411
root <root@DESKTOP-R7S4241>
2023-02-16 14:58:06 +0100

    Inputs

---
+++ Input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Variables and Input
+ * Let's gooooo
+ */
+int main(void) 
+{
+	char name[100];
+
+	printf("Enter a name\n");
+	scanf("%s", name);
+	printf("Your name is %s", name);
+	
+
+
+		
+
+
+
+	return(0);
+}
