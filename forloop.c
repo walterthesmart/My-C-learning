793c851ec0037ea77d4e2456549bfcbce8d97385
Walterthesmart <nwaugowalter@gmail.com>
2023-02-18 01:48:20 -0500

    for loops

---
+++ forloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		printf("Hello, my friend\n");
+	}
+	for (int j = 0; j <= 20; j++)
+	{
+		printf("Number %d\n", j);
+	}
+	return (0);
+}
