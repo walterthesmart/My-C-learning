b2d9c50e2f9a8022b02e4a99eabf8d89bacf0eac
Walterthesmart <nwaugowalter@gmail.com>
2023-03-01 00:52:17 -0500

    Use the funky header

---
+++ funky.h
@@ -0,0 +1,6 @@
+#ifndef FUNKY_H_
+#define FUNKY_H_
+
+int add(int num1, int num2);
+
+#endif
--- header.c
+++ header.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
+#include "funky.h"
 
+
+int main()
+{
+	int result = add(6, 4);
+	printf("Result is : %d\n", result);
+	return (0);
+}
